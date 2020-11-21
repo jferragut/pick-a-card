f9c17985a4008592b47917dc4ddc360dac7dc2b7
Jonathan Ferragut <jonathan@alkemydev.com>
2020-11-21 23:26:37 +0000

    modify value array

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ function moveCard() {
 }
 
 let suites = ["club", "spade", "heart", "diamond"];
-let values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let values = [0, "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
 function changeCard() {
   let value = values[getRndInteger(0, values.length)];
